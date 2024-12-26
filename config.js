410d7848c0d2c207a03906d124408d86d215cdc7
Allumodex <spm11347@gmail.com>
2024-12-26 14:42:49 +0530

    Update config.js

--- config.js
+++ config.js
@@ -26,10 +26,10 @@ function convertToBool(text, fault = 'true') {
 //--------------------------Mr Frank---------------------------------------
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID || "ENTER YOUR SUBZERO SESSION HERE",
+SESSION_ID: process.env.SESSION_ID || "PJQGHbjS#l28gWpOT617NSHrhSD6M1XCJe1pU4LEaMYe4OihtnN4",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", //TRUE OR FALSE
 OWNER_NUMBER: process.env.OWNER_NUMBER || "242067274660",
-MODE: process.env.MODE || "public",  // PRIVATE OR PUBLIC
+MODE: process.env.MODE || "private",  // PRIVATE OR PUBLIC
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
 AUTO_STICKER: process.env.AUTO_STICKER || "false",
 AUTO_REPLY: process.env.AUTO_REPLY || "false",
@@ -42,7 +42,7 @@ FAKE_RECORDING: process.env.FAKE_RECORDING || "false",
 AUTO_REACT: process.env.AUTO_REACT || "false",
 HEART_REACT: process.env.HEART_REACT || "false",
 OWNER_REACT: process.env.OWNER_REACT || "false",
-BOT_NAME: process.env.BOT_NAME || "『 ＪＩＮＨＵＹＫ-ＭＤ-Ｖ２ 』",
+BOT_NAME: process.env.BOT_NAME || "『 queen jesna -Ｖ２ 』",
 OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39", // omdbapi.com
 };
 
